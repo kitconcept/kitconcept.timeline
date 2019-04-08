a1085f76cd5685afd4617936fd578ebb32cbcebb
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2019-04-08 18:33:55 -0300

    Create content types

--- src/collective/timeline/interfaces.py
+++ src/collective/timeline/interfaces.py
@@ -1,12 +1,34 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
-from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from collective.timeline import _
+from plone.namedfile import field
 from plone.supermodel import model
+from zope import schema
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class ICollectivetimelineCoreLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
 
 
-class IHomepage(model.Schema):
-    """Homepage content type interface."""
+class ITimeline(model.Schema):
+    """Timeline content type interface."""
+
+
+class ITimelineEntry(model.Schema):
+    """Timeline Item content type interface."""
+
+    image = field.NamedBlobImage(
+        title=_(u'Image'),
+        required=False,
+    )
+
+    copyright = schema.TextLine(
+        title=_(u'Copyright'),
+        required=False,
+    )
+
+    date = schema.Datetime(
+        title=_(u'Date'),
+        required=False,
+    )
