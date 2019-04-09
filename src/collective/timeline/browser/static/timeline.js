1217b87697eb7740db69f514f86a64d00f558333
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2019-04-09 10:26:50 -0300

    Tidy up code; Change the bullet

--- src/collective/timeline/browser/static/timeline.js
+++ src/collective/timeline/browser/static/timeline.js
@@ -1,16 +1,19 @@
-$(document).ready(function(){
-    var my_posts = $("[rel=tooltip]");
+$(function() {
+  var my_posts = $("[rel=tooltip]");
+  var size = $(window).width();
 
-    var size = $(window).width();
-    for(i=0;i<my_posts.length;i++){
-        the_post = $(my_posts[i]);
+  for (i=0; i < my_posts.length; i++) {
+    the_post = $(my_posts[i]);
 
-        if(the_post.hasClass('invert') && size >=767 ){
-            the_post.tooltip({ placement: 'left'});
-            the_post.css("cursor","pointer");
-        }else{
-            the_post.tooltip({ placement: 'rigth'});
-            the_post.css("cursor","pointer");
-        }
+    if (the_post.hasClass('invert') && size >= 767 ) {
+      the_post.tooltip({
+        placement: 'left',
+      });
+    } else {
+      the_post.tooltip({
+        placement: 'right',
+      });
     }
+    the_post.css('cursor', 'pointer');
+  }
 });
