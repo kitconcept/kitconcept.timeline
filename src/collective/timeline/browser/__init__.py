09e8ccf1f99ae23a14c30b7e6038b192bb780aa8
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2019-04-09 01:05:46 -0300

    Customize timeline view

--- src/collective/timeline/browser/__init__.py
+++ src/collective/timeline/browser/__init__.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+from plone import api
+from plone.dexterity.browser.view import DefaultView
+
+
+class TimelineView(DefaultView):
+    """Defaul view for Timeline content type."""
+    
+    def results(self):
+        return api.content.find(
+            context=self.context,
+            depth=1,
+            portal_type='Timeline Entry',
+        )
