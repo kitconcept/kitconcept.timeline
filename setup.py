eb58902fb0818800868d24f7e57863e86e8e4c0a
Timo Stollenwerk <stollenwerk@kitconcept.com>
2019-04-17 13:50:48 +0200

    Add description.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = '\n\n'.join([
 setup(
     name='kitconcept.timeline',
     version='1.0.0a1',
-    description='',
+    description='Plone add-on that allows to create a timeline.',
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
