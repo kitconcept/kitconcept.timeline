d33f71e41d6b39544df08ed268c99e2e651cf4ea
Timo Stollenwerk <stollenwerk@kitconcept.com>
2019-06-04 06:12:09 +0200

    Back to development: 1.0.0a5
    [ci skip]

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="kitconcept.timeline",
-    version="1.0.0a4",
+    version="1.0.0a5.dev0",
     description="Plone add-on that allows to create a timeline.",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
